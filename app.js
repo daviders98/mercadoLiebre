4b332807a65e8904132887a43bba3ba89b6eb7b9
Juventino Aguilar <juve@juve-laptop.attlocal.net>
2021-06-17 17:50:20 -0700

    Se agregaron las paginas de LOgin y Register

--- app.js
+++ app.js
@@ -8,6 +8,16 @@ app.listen(3010, () => {
 
 app.use(express.static(path.resolve(__dirname, './public')))
 
+app.get("/register", (req,res) => {
+    let rutaAbsoluta = path.resolve(__dirname,"./views/register.html");
+    res.sendFile(rutaAbsoluta);
+})
+
+app.get("/login", (req,res) => {
+    let rutaAbsoluta = path.resolve(__dirname,"./views/login.html");
+    res.sendFile(rutaAbsoluta);
+})
+
 app.get('/', (req,res) =>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
 })
